4f4bf022cdc04657b3055c3554d58cd3b514a63c
ismoilhukumatov <ismoilhukumatov9990@gmail.com>
2023-04-05 17:00:02 -0700

    add theme in project and make tablayout with viewpager2 in part menu

--- app/src/main/java/com/example/tztcell/presentation/fragment/main/MainFlowFragment.kt
+++ app/src/main/java/com/example/tztcell/presentation/fragment/main/MainFlowFragment.kt
@@ -1,12 +1,17 @@
 package com.example.tztcell.presentation.fragment.main
 
 import androidx.navigation.NavController
+import androidx.navigation.ui.setupWithNavController
+import by.kirich1409.viewbindingdelegate.viewBinding
 import com.example.tztcell.R
+import com.example.tztcell.databinding.FragmentFlowMainLayoutBinding
 import com.example.tztcell.presentation.base.BaseFlowFragment
 
 class MainFlowFragment: BaseFlowFragment(R.layout.fragment_flow_main_layout,R.id.fragment_container_view){
+    private val binding by viewBinding(FragmentFlowMainLayoutBinding::bind)
+
     override fun setupNavigation(navController: NavController) {
         super.setupNavigation(navController)
-        //NavigationUI.setupWithNavController(requireActivity().findViewById(R.id.toolbar),navController,null)
+       binding.bottomNavigation.setupWithNavController(navController)
     }
 }
--- app/src/main/java/com/example/tztcell/presentation/fragment/main/menu/MenuFragment.kt
+++ app/src/main/java/com/example/tztcell/presentation/fragment/main/menu/MenuFragment.kt
@@ -4,7 +4,21 @@ import by.kirich1409.viewbindingdelegate.viewBinding
 import com.example.tztcell.R
 import com.example.tztcell.databinding.FragmentMenuBinding
 import com.example.tztcell.presentation.base.BaseFragment
+import com.example.tztcell.presentation.fragment.main.menu.viewpager.ViewPagerAdapter
+import com.google.android.material.tabs.TabLayoutMediator
 
 class MenuFragment:BaseFragment<FragmentMenuBinding>(R.layout.fragment_menu) {
     override val binding by viewBinding(FragmentMenuBinding::bind)
+
+    override fun initialize() {
+        super.initialize()
+        binding.viewPager.adapter =
+            ViewPagerAdapter(requireActivity().supportFragmentManager, lifecycle)
+        TabLayoutMediator(binding.tabLayout, binding.viewPager) { tab, position ->
+            when(position){
+                0 -> tab.text = resources.getString(R.string.foryou)
+                1 -> tab.text = resources.getString(R.string.following)
+            }
+        }.attach()
+    }
 }
